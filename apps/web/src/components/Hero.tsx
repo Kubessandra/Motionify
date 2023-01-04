42952c5ce403cb8e6f095377c9e2ff60f141f01e
Kubessandra <aymeric.beudaert@gmail.com>
2023-01-04 22:57:02 +0100

    feat: update Hero and Logo
    .

---
+++ apps/web/src/components/Hero.tsx
@@ -0,0 +1,273 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+import { useSession } from "~/hooks/useSession";
+import { Logo } from "./Logo";
+import Link from "next/link";
+import auth from "~/utils/auth";
+import securedRoutes from "~/utils/routing";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+export default function Hero() {
+  const session = useSession(false);
+
+  return (
+    <div className="bg-gray-50">
+      <div className="relative overflow-hidden">
+        <div className="absolute inset-y-0 h-full w-full" aria-hidden="true">
+          <div className="relative h-full">
+            <svg
+              className="absolute right-full translate-y-1/3 translate-x-1/4 transform sm:translate-x-1/2 md:translate-y-1/2 lg:translate-x-full"
+              width={404}
+              height={784}
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern
+                  id="e229dbec-10e9-49ee-8ec3-0286ca089edf"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={784}
+                fill="url(#e229dbec-10e9-49ee-8ec3-0286ca089edf)"
+              />
+            </svg>
+            <svg
+              className="absolute left-full -translate-y-3/4 -translate-x-1/4 transform sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
+              width={404}
+              height={784}
+              fill="none"
+              viewBox="0 0 404 784"
+            >
+              <defs>
+                <pattern
+                  id="d2a68204-c383-44b1-b99f-42ccff4e5365"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={784}
+                fill="url(#d2a68204-c383-44b1-b99f-42ccff4e5365)"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div className="relative pt-6 pb-16 sm:pb-24">
+          <Popover>
+            <div className="mx-auto max-w-7xl px-6">
+              <nav
+                className="relative flex items-center justify-between sm:h-10 md:justify-center"
+                aria-label="Global"
+              >
+                <div className="flex flex-1 items-center md:absolute md:inset-y-0 md:left-0">
+                  <div className="flex w-full items-center justify-between md:w-auto">
+                    <Logo size={64} />
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <Popover.Button className="inline-flex items-center justify-center rounded-md bg-gray-50 p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="hidden md:flex md:space-x-10">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="hidden md:absolute md:inset-y-0 md:right-0 md:flex md:items-center md:justify-end">
+                  <span className="inline-flex rounded-md shadow">
+                    {!session ? (
+                      <Link
+                        href={auth.loginURL}
+                        className="inline-flex items-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-indigo-600 hover:text-indigo-500"
+                      >
+                        Log in
+                      </Link>
+                    ) : (
+                      <Link
+                        href={securedRoutes.DASHBOARD.path}
+                        className="inline-flex items-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-indigo-600 hover:text-indigo-500"
+                      >
+                        Dashboard
+                      </Link>
+                    )}
+                  </span>
+                </div>
+              </nav>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="duration-150 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
+              >
+                <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+                  <div className="flex items-center justify-between px-5 pt-4">
+                    <Logo size={16} />
+                    <div className="-mr-2">
+                      <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Close main menu</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="space-y-1 px-2 pt-2 pb-3">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  {!session ? (
+                    <Link
+                      href={auth.loginURL}
+                      className="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100 hover:text-indigo-700"
+                    >
+                      Log in
+                    </Link>
+                  ) : (
+                    <Link
+                      href={securedRoutes.DASHBOARD.path}
+                      className="block w-full bg-gray-50 px-5 py-3 text-center font-medium text-indigo-600 hover:bg-gray-100 hover:text-indigo-700"
+                    >
+                      Dashboard
+                    </Link>
+                  )}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-24">
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block">Data to enrich your</span>
+                <span className="block text-indigo-600">online business</span>
+              </h1>
+              <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                fugiat aliqua.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative">
+          <div className="absolute inset-0 flex flex-col" aria-hidden="true">
+            <div className="flex-1" />
+            <div className="w-full flex-1 bg-gray-800" />
+          </div>
+          <div className="mx-auto max-w-7xl px-6">
+            <img
+              className="relative rounded-lg shadow-lg"
+              src="https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg"
+              alt="App screenshot"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-800">
+        <div className="mx-auto max-w-7xl py-16 px-6 sm:py-24 lg:px-8">
+          <h2 className="text-center text-base font-semibold text-gray-400">
+            Trusted by over 26,000 forward-thinking companies
+          </h2>
+          <div className="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
+            <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+                alt="Tuple"
+              />
+            </div>
+            <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
+                alt="Mirage"
+              />
+            </div>
+            <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+                alt="StaticKit"
+              />
+            </div>
+            <div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+                alt="Transistor"
+              />
+            </div>
+            <div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+              <img
+                className="h-12"
+                src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
+                alt="Workcation"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- apps/web/src/pages/dashboard.tsx
+++ apps/web/src/pages/dashboard.tsx
@@ -1,10 +1,9 @@
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import Layout from "~/components/Layout";
 import { useSession } from "~/hooks/useSession";
 import VideoListing from "~/components/VideoListing";
 
 const Dashboard = () => {
-  const [value, setValue] = useState("");
   const session = useSession(true);
   if (!session) return null;
 
@@ -12,13 +11,6 @@ const Dashboard = () => {
     <div>
       <h1 className="text-2xl font-bold my-8">Welcome to the dashboard</h1>
       <VideoListing />
-      <div className="flex flex-col items-center space-y-4 m-8">
-        <input
-          className="border border-indigo-200"
-          value={value}
-          onChange={(e) => setValue(e.currentTarget.value)}
-        />
-      </div>
     </div>
   );
 };
